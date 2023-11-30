541ba45f9c5b37b261e0ec820a9b023b4bf67961
soboli.ucho <soboli.ucho@gmail.com>
2023-11-30 15:36:14 +0100

    změna ikonek a smazání bordelu

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $conn = conenect_to_database_kameny();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- <link rel="icon" href="https://www.muzeum-boskovicka.cz/sites/default/files/favicons/favicon-16x16.png"> -->
-  <link rel="icon" href="images/icona.jpg">
+  <link rel="icon" href="https://www.muzeum-boskovicka.cz/sites/default/files/favicons/favicon-16x16.png">
+  <!-- <link rel="icon" href="images/icona.jpg"> -->
 
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/mapacz.css">
