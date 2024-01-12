baae0d18c0ede8fd6392a68a0c89255d6f90c272
SoboliUcho <126193245+SoboliUcho@users.noreply.github.com>
2024-01-12 01:14:15 +0100

    Update _login.php

--- _login.php
+++ _login.php
@@ -2,8 +2,8 @@
 if (isset($_POST['log_confirm'])) { //pokud byl odeslán přihlašovací formulář
     $jmeno = $_POST['jmeno']; //vem jeho data
     $heslo = $_POST['heslo'];
-    if ($jmeno == "stolpersteine" && $heslo=="kamen333"){
-        setcookie("prihlaseni", $jmeno, time() + 60 * 60 * 24 * 90, "/");
+    if ($jmeno == "jmeno" && $heslo=="heslo"){
+        setcookie("login", $jmeno, time() + 60 * 60 * 24 * 90, "/");
         // setcookie("prihlaseni", $jmeno, time() + 60 , "/");
 
     }
