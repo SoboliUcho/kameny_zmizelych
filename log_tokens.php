7504cf06fc0bcabd3bcaa260074fba927153201e
SoboliUcho <126193245+SoboliUcho@users.noreply.github.com>
2024-01-12 01:12:57 +0100

    Update log_tokens.php

--- log_tokens.php
+++ log_tokens.php
@@ -1,17 +1,13 @@
 <?php
 $database = array(
-    // "servername" => "localhost",
-    // "username" => "root",
-    // "password" => "",
-    // "dbname" => "kameny",
-    "servername" => "md380.wedos.net",
-    "username" => "a331056_kameny",
-    "password" => "jSwfrnh4",
-    "dbname" => "d331056_kameny",
+    "servername" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "dbname" => "kameny",
 );
 $darujme_api = array(
-    "apiId" => 74234503,
-    "apiSecret" => "g9qesn0jllgllrlyzv3umwd1k4y4d8wiwstsrglq",
-    "organizationId" => 1200707,
+    "apiId" => 1111111,
+    "apiSecret" => "apiSecret",
+    "organizationId" => 111111,
 )
     ?>
