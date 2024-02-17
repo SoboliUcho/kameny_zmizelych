4d37657ebb3c9c11b428be7a78e6806d5bcde997
swpv <swpv@email.cz>
2024-02-17 18:02:34 +0100

    hesla

--- log_tokens.php
+++ log_tokens.php
@@ -4,18 +4,15 @@ $database = array(
     "username" => "root",
     "password" => "",
     "dbname" => "kameny",
-    // "servername" => "md380.wedos.net",
-    // "username" => "a331056_kameny",
-    // "password" => "jSwfrnh4",
-    // "dbname" => "d331056_kameny",
+
 );
 $darujme_api = array(
-    "apiId" => 74234503,
-    "apiSecret" => "g9qesn0jllgllrlyzv3umwd1k4y4d8wiwstsrglq",
-    "organizationId" => 1200707
+    "apiId" => 11111,
+    "apiSecret" => "apiSecret",
+    "organizationId" => 11111
 );
 $mapy_api = array(
-    "api_key" => "-YU-3m6kTF_X0RFCcyIDyMT5EbJixEkzsmz8JlWMoWY"
+    "api_key" => "-YU-api_key"
 );
 $pozice = array(
     array(
